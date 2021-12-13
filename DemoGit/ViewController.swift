320bcddc06014eadf8bea848255e28d78735820c
mborade46 <88447877+mborade46@users.noreply.github.com>
2021-12-12 21:42:30 -0500

    added tableview methods

--- DemoGit/ViewController.swift
+++ DemoGit/ViewController.swift
@@ -10,14 +10,33 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var fruitsTableview: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        fruitsTableview.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         
         
     }
+ 
 
 
+}
+
+extension ViewController : UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for:indexPath)
+        cell.textLabel?.text = "\(indexPath.row+1)"
+        return cell
+    }
+    
+    
 }
 
