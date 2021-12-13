4020bc6227ceed502953c5a2a8d50085eacee198
mborade46 <88447877+mborade46@users.noreply.github.com>
2021-12-12 21:29:55 -0500

    fruitsout tableview

--- DemoGit/ViewController.swift
+++ DemoGit/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fruitsTableview: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
     }
 
 
